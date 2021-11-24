58eb29dc7c4c9cb4c4b5634d55ddeda5333fd486
Luc-Saccoccio <lucsaccoccio@disroot.org>
2021-11-24 21:48:13 +0100

    Almost final version

--- prologin/2022/qualifications/PB3.hs
+++ prologin/2022/qualifications/PB3.hs
@@ -18,21 +18,21 @@ import qualified Data.Set           as Set (Set, foldr, fromList, member,
 type SubList = (Int, Int)
 -- Tree Key-(length of list, list)
 type DivList = IntMap SubList
-
+-- Set of divisors
 type DivSet = Set.Set Int
 
 {- Problem Solution -}
 -- Rewritting Show instance is too complex...
 showL :: Show a => [a] -> String
 showL []     = ""
-showL [x]    = show x
+showL [x]    = show x ++ "\n"
 showL (x:xs) = show x ++ " " ++ showL xs
 
--- Return the difference between the second and the first element
+-- Return the difference between the second and the first element. Pointfree style
 diff :: SubList -> Int
 diff = (+1) . liftM2 (-) snd fst
 
--- Integer sqrt
+-- Integer sqrt. Pointfree style
 isqrt :: Int -> Int
 isqrt = round . (sqrt::Double->Double) . fromIntegral
 
@@ -50,29 +50,31 @@ divisors n = (foldr (go . (head &&& length)) [1] . group) $ fac (abs n) 2
 -- Return a slice of a list
 slice :: Int -> Int -> [a] -> [a]
 slice i k xs | i>0 = take (k-i+1) $ drop (i-1) xs
-slice _ _ _ = error "Alors non"
+slice _ _ _ = error "Alors non" -- Nope. Won't happen.
 
 -- Return the longest sublist for each divisor, in a map (i.e. dictionnary)
 semiValidSubLists :: Int -> DivSet -> [Int] -> DivList
-semiValidSubLists n divs l = go slices
+semiValidSubLists n divs l = foldr alter empty slices
     where
+        -- All possible slices
         slices :: [SubList]
         slices = [(j, i) | i <- [0..n], j <- [1..i]]
 
-        go :: [SubList] -> DivList
-        go []     = empty
-        go ((start, finish):xs) =
+        -- Alter or not the tree (by inserting the sublist)
+        alter :: SubList -> DivList -> DivList
+        alter (start, finish) =
             let s = sum (slice start finish l) in
                 if abs s `Set.member` divs then
-                    insertWith cmp s (start, finish) $ go xs
+                    insertWith cmp s (start, finish)
                 else
-                    go xs
+                    id
 
+        -- Compare two sublists and return the longest
         cmp :: SubList -> SubList -> SubList
-        cmp (sA, fA) (sB, fB) =
-            let lenA = fA - sA + 1
-                lenB = fB - sB + 1 in
-            if lenA > lenB then (sA, fA) else (sB, fB)
+        cmp l1 l2 =
+            let len1 = diff l1
+                len2 = diff l2 in
+            if len1 > len2 then l1 else l2
 
 -- Return the two best lists
 bestLists :: Int -> DivSet -> DivList -> (SubList, SubList)
@@ -95,17 +97,16 @@ formatSolution list (s1, s2) =
         len2 = diff s2
         list1 = slice' s1
         list2 = slice' s2
-    in if len1 > len2 then
-        showL list1 ++ "\n" ++ showL list2
-    else if len1 < len2 then
-        showL list2 ++ "\n" ++ showL list1
-    else
-        if sum list1 > sum list2 then
-            showL list1 ++ "\n" ++ showL list2
-        else
-            showL list2 ++ "\n" ++ showL list1
+    in case compare len1 len2 of -- Order of printing according to lists length
+         GT -> showL list1 ++ showL list2
+         LT -> showL list2 ++ showL list1
+         EQ -> if sum list1 > sum list2 then -- Compute the sums. There is a probably a clever method (storing the value ?)
+                    showL list1 ++ showL list2
+                else
+                    showL list2 ++ showL list1
 
     where
+        -- Pointfree style (inversed argument and accepts a tuple) slice function
         slice' :: SubList -> [Int]
         slice' = flip (uncurry slice) list
 
@@ -120,8 +121,12 @@ solution rawInput =
                 formatSolution list (fromJust (div0!?0), (0, n))
     else
         let lists = semiValidSubLists n divs list in
-            formatSolution list $ bestLists x divs lists -- Treat case were no bestLists is returned
+            case bestLists x divs lists of
+              ((0, 0), (0, 0)) -> "IMPOSSIBLE"
+              sub -> formatSolution list sub
+
     where
+        -- List of number making the input
         input :: [Int]
         input = map (read @Int) . words $ rawInput
 
