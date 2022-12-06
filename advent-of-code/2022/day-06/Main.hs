0eb8aba27333279fc80f67d70351059b934fc7fc
Luc-Saccoccio <lucsaccoccio@disroot.org>
2022-12-06 06:59:02 +0100

    Monadic operators are expensive :'(

--- advent-of-code/2022/day-06/Main.hs
+++ advent-of-code/2022/day-06/Main.hs
@@ -1,25 +1,22 @@
 module Main where
 
-import Control.Arrow ((&&&))
-import Witherable (hashNub)
+import           Control.Arrow ((&&&))
+import           Witherable    (hashNub)
 
-go :: [Char] -> Int -> String -> Int
-go _ n [] = n
-go l n (x:xs) =
-  let l' = if length l < 4 then (x:l) else x:init l
-   in if length (hashNub l') == 4 then n else go l' (n+1) xs
-
-go2 :: [Char] -> Int -> String -> Int
-go2 _ n [] = n
-go2 l n (x:xs) =
-  let l' = if length l < 14 then (x:l) else x:init l
-   in if length (hashNub l') == 14 then n else go2 l' (n+1) xs
+aux :: Int -> String -> Int
+aux n s = go 1 (take n s) s
+  where
+    go :: Int -> [Char] -> String -> Int
+    go i _ [] = i
+    go i l (x:xs) =
+      let l' = x : init l
+       in if length (hashNub l') == n then i else go (i+1) l' xs
 
 part1 :: String -> Int
-part1 = (+1) . go [] 0
+part1 = aux 4
 
 part2 :: String -> Int
-part2 = (+1) . go2 [] 0
+part2 = aux 14
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2)
