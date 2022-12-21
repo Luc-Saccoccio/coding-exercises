9a65b944009c3e31e0dc4829d420158775ccf0cb
Luc-Saccoccio <lucsaccoccio@disroot.org>
2022-12-21 17:07:11 +0100

    AoC 2022 day 20 format (painfully slow)

--- advent-of-code/2022/day-20/Main.hs
+++ advent-of-code/2022/day-20/Main.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE StrictData       #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE StrictData #-}
+
 module Main where
 
-import Control.Arrow ((&&&))
-import Data.List (foldl')
-import Data.Sequence (Seq (..))
+import           Control.Arrow ((&&&))
+import           Data.List     (foldl')
+import           Data.Sequence (Seq (..))
 import qualified Data.Sequence as S
 
 type CList = Seq (Int, Int)
@@ -32,16 +33,16 @@ move (x@(_, n) :<| xs) =
    in x :<| rs <> ls
 
 mixing :: CList -> CList
-mixing cl = foldl' (\acc n -> move $ focusIndex n acc) cl [1..length cl]
+mixing cl = foldl' (\acc n -> move $ focusIndex n acc) cl [1 .. length cl]
 
 indexes :: CList -> [Int]
 indexes xs = map (snd . S.index xs . (`mod` length xs)) [1000, 2000, 3000]
 
 part1 :: String -> Int
-part1 = sum . indexes . focusValue 0 . mixing . S.fromList . zip [1..] . parse
+part1 = sum . indexes . focusValue 0 . mixing . S.fromList . zip [1 ..] . parse
 
 part2 :: String -> Int
-part2 = sum . indexes . focusValue 0 . (!!10) . iterate mixing . S.fromList . zip [1..] . map (*811589153) . parse
+part2 = sum . indexes . focusValue 0 . (!! 10) . iterate mixing . S.fromList . zip [1 ..] . map (* 811589153) . parse
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2)
