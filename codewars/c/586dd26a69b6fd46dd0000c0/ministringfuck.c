c024fc698f85081b1e4242422ebf2ab4943f579f
Luc-Saccoccio <lucsaccoccio@disroot.org>
2025-01-13 17:39:06 +0100

    Kata C MiniStringFuck fixed

--- codewars/c/586dd26a69b6fd46dd0000c0/ministringfuck.c
+++ codewars/c/586dd26a69b6fd46dd0000c0/ministringfuck.c
@@ -2,25 +2,20 @@
 
 char *my_first_interpreter (const char *code)
 {
-    int output_len = 64, diff;
-    char *output = calloc(output_len, sizeof(char));
+    int output_len = 0;
+    for (char const* i = code; *i; i++)
+        if (*i=='.')
+            output_len++;
+    char *output = malloc(output_len * sizeof(char));
     char *current = output;
     unsigned char n = 0;
     while (*code) {
         switch (*code) {
             case '+': n++; break;
-            case '.':
-                if ((diff = current - output) == (output_len - 1)) {
-                    output_len += 64;
-                    output = realloc(output, output_len);
-                    current = output + diff;
-                }
-                *current = (char)n;
-                current++;
-                break;
+            case '.': *current++ = (char)n; break;
         }
         code++;
     }
-    *(current+1) = 0;
+    *current = 0;
     return output;
 }
